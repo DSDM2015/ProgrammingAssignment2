36e4000c96ade033b7691a42016ea0fd5e87401d
DSDM2015 <Andreas Warntjen@users.noreply.github.com>
2015-10-15 12:01:46 +0200

    Changed in line with assignment 2 requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate and return the inverse of a matrix x
+## To save computing time, a cached value is used if the computation
+## has already been done earlier
+## Uses superassignment operator (<--) to assign values to variables
+## created in other environments than the current function
 
-## Write a short comment describing this function
+## Creates a list containing 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## If already cached, it will return the cached value (and print out a message accordingly)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x["getinverse"]
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x["get"] 
+  m <- solve(x, ...)
+  x["setinverse"]
+  m
 }
